641ad449c75b4a5d7de70dba91589856c2afb28e
gtrdotmcs <gtr.mcs@gmail.com>
2015-02-19 07:04:22 +0530

    modifid code for claverbot chat to wait to elem present and carrry on and comment part web site chages it behaviour

--- pythonbotchat/cleverbot/cleverbot_chat.py
+++ pythonbotchat/cleverbot/cleverbot_chat.py
@@ -4,6 +4,9 @@ It is very simple selnium based code which cat with clevarbot and genrate log fr
 import time
 from datetime import datetime
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def botcollectlog(url,name):
     browser = webdriver.Firefox()
@@ -12,18 +15,19 @@ def botcollectlog(url,name):
     writelog = open(name_of_log,'w')
     # below list will change after based on db creation current it is static.list should have at least 20-25 questions
     list_q = ["how are you?" ,"are you good thinker?", "well me absolutly fine ","yes","i like know more about you"]
-    submit = browser.find_element_by_css_selector("input.bb#sayit")
-    submit.click()
-    submit = browser.find_element_by_css_selector(".obscure.extend.show")
-    submit.click()
+    #submit = browser.find_element_by_css_selector("input.bb#sayit")
+    #submit.click()
+    #submit = browser.find_element_by_css_selector(".obscure.extend.show")
+    #submit.click()
     for line in list_q:
         input = browser.find_element_by_id("stimulus")
         input.send_keys(line)
         time.sleep(2)
         submit = browser.find_element_by_css_selector("input.bb#sayit")
         submit.click()
-        writelog.write("["+time.strftime("%H:%M:%S:%s")+"]code: "+line+"\n") 
-        time.sleep(5)
+        writelog.write("["+time.strftime("%H:%M:%S:%s")+"]code: "+line+"\n")
+        wait = WebDriverWait(browser, 40)
+        element = wait.until(EC.presence_of_element_located((By.ID, "snipTextIcon")))
         text = browser.find_element_by_id("typArea")
         while text.text.strip() == "":
           time.sleep(1)
